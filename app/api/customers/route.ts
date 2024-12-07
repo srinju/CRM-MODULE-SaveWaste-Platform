f49a53bf2c11a71f16f38d15f1173e7239c887d4
SRINJOY DAS <srinjoydas1104@gmail.com>
2024-12-07 05:38:15 +0530

    i am the fucking best

--- app/api/customers/route.ts
+++ app/api/customers/route.ts
@@ -1,15 +1,21 @@
 import { NextResponse } from "next/server"
 import { PrismaClient } from "@prisma/client"
+import { getServerSession } from "next-auth"
+import { authOptions } from "@/lib/auth"
 
 const prisma = new PrismaClient()
 
 export async function POST(req: Request) {
+  const session = await getServerSession(authOptions);
+  if(!session){
+    return null;
+  }
   try {
     const body = await req.json()
     const customer = await prisma.customer.create({
       data: {
         ...body,
-        userId: 1 , // TODO: Get from auth session
+        userId: session.user.id , // TODO: Get from auth session
       },
     })
     return NextResponse.json(customer)
